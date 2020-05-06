11f9d6cecc1ce614574b73c5061477c15055b0b8
Scorpie <rautan_97@live.com>
2020-05-06 11:43:12 +0200

    Memory Class with internal tests done
    zdrgdrghdfght

---
+++ CalculatorBackend/Memory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorBackend
+{
+    public class Memory
+    {
+        public decimal Value { get; private set; }        
+
+        public void MemoryAdd(decimal value)
+        {
+            this.Value += value;
+        }
+    
+        public void MemorySubtract(decimal value)
+        {
+            this.Value -= value; 
+        }
+
+        public void MemorySave(decimal value)
+        {
+            this.Value = value;
+        }
+
+        public void MemoryClear()
+        {
+
+            this.Value = 0;
+        }
+    }
+}
---
+++ CalculatorBackendTest/UnitTestMem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorBackend;
+using System.Collections.Generic;
+
+namespace CalculatorBackendTest
+{
+    [TestClass]
+    public class UnitTestMem
+    {
+        private Memory mem;
+        private const decimal EXPECTEDNUM = 354m;
+        private const decimal NUM = 354m;
+        private const decimal SUBVALUE = 708m;
+        private const decimal SAVENUM = 652m;
+        private const decimal ZERO = 0;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            mem = new Memory();
+        }
+
+
+        [TestMethod]
+        public void MemoryAddTest()
+        {
+            mem.MemoryAdd(NUM);
+
+            Assert.AreEqual(EXPECTEDNUM, mem.Value);
+        }
+
+        [TestMethod]
+        public void MemorySubtractTest()
+        {
+            mem.MemoryAdd(SUBVALUE);
+            mem.MemorySubtract(NUM);
+
+            Assert.AreEqual(EXPECTEDNUM, mem.Value);
+        }
+
+        [TestMethod]
+        public void MemorySaveTest()
+        {
+            mem.MemorySave(SAVENUM);
+
+            Assert.AreEqual(SAVENUM, mem.Value);
+        }
+
+        [TestMethod]
+        public void MemoryClearTest()
+        {
+            mem.MemoryClear();
+
+            Assert.AreEqual(ZERO, mem.Value);
+        }
+    }
+}
